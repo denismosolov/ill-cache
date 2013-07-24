842250e003b4cb33689d6876cd2c8a03927b66bf
Denis Mosolov <denismosolov@gmail.com>
2013-07-24 16:47:29 +0400

    Added documentation to \Ill\Cache\Memcached

--- src/Ill/Cache/Memcached.php
+++ src/Ill/Cache/Memcached.php
@@ -4,12 +4,12 @@ namespace Ill\Cache;
 
 class Memcached {
 	
-	/*
+	/**
 	 * Default expired time for keys
 	 * number of seconds in 30 days, read http://www.php.net/manual/en/memcached.expiration.php before change the value
 	 */
 	const DEFAULT_EXPIRED_TIME = 2592000;
-
+	
 	private $_m;
 
 	private $_lastSavedKey;
@@ -28,14 +28,22 @@ class Memcached {
 		$this->_tagger = new \Ill\Cache\Tagger($memcached);
 	}
 
-	public function set($key, $value, $tags = array(), $expired = self::DEFAULT_EXPIRED_TIME) {
+	/**
+	 * Stores the $value on a memcache server under the specified $key. 
+	 * @param string $key The key under which to store the value
+	 * @param mixed The value to store. Can be any valid PHP type except for resources, because those cannot be represented in a serialized form.
+	 * @param array $tags can be set of string tags assigned to $data or empty array if no tags assigned
+	 * @param int $expiration Can be used to control when the value is considered expired. Defaults to one month
+	 * @return boolean Returns TRUE on success or FALSE on failure
+	 */
+	public function set($key, $value, $tags = array(), $expiration = self::DEFAULT_EXPIRED_TIME) {
 		$_tags = array();
 		foreach ($tags as $tag) {
 			$_tag = new \Ill\Cache\Tag($tag);
 			$this->_tagger->register($_tag);
 			$_tags[] = $_tag;
 		}
-        if ($this->_m->set($key, new \Ill\Cache\Container($value, $_tags), $expired)) {
+        if ($this->_m->set($key, new \Ill\Cache\Container($value, $_tags), $expiration)) {
 			$this->_lastSavedValue = $value;
 	        $this->_lastSavedKey = $key;
 	        $this->_lastSavedTags = $tags;
@@ -45,6 +53,11 @@ class Memcached {
 	    }
 	}
 
+	/**
+	 * Returns the item that was previously stored under the key. 
+	 * @param string $key The key of the item to retrieve.
+	 * @return mixed Returns the value stored in the cache or FALSE otherwise.
+	 */
 	public function get($key) {
 		$container = $this->_m->get($key);
 		if ($container instanceof \Ill\Cache\Container) {
@@ -59,6 +72,11 @@ class Memcached {
 		}
 	}
 
+	/**
+	 * Flush all data which was previously stored with $tag assigned.
+	 * @param string $tag
+	 * @return boolean Returns TRUE on success or FALSE on failure
+	 */
 	public function incTagVersion($tag) {
 		$_tag = new \Ill\Cache\Tag($tag);
 		$_tag->setVersion(new \Ill\Cache\Version()); // inc version
